70d6d22465d0626b6e20859f45c39f7c57c24f61
otoone_dev <otoone.dev@gmail.com>
2025-12-30 21:21:32 +0900

    PlatformIO版、I2CによるLPS33制御とATOMS3の画面制御を追加

--- afuue_project/PlatformIO/AFUUE2_nsys/src/AFUUE_nsys.cpp
+++ afuue_project/PlatformIO/AFUUE2_nsys/src/AFUUE_nsys.cpp
@@ -1,12 +1,26 @@
 #include <Arduino.h>
-
+#include <M5Unified.h>
 #define CORE0 (0)
 #define CORE1 (1)
 
+//-----------
+#include <Wire.h>
+#define I2CPIN_SDA (38)
+#define I2CPIN_SCL (39)
+#define I2C_FREQ (400000)
+
+#define LPS33_ADDR (0x5C)
+#define LPS33_WHOAMI_REG (0x0F)
+#define LPS33_WHOAMI_RET (0xB1)
+#define LPS33_REG_CTRL (0x10)
+#define LPS33_REG_OUT_XL (0x28)
+
+//-----------
 #include <USB.h>
 #include <USBMIDI.h>
 USBMIDI MIDI("AFUUE2R");
  
+//-----------
 #include <FastLED.h>
 #define LED_PIN   (35)
 #define NUM_LEDS  (1)
@@ -18,8 +32,6 @@ void setLed(CRGB color) {
   leds[0] = color;
   FastLED.show();
 }
-
-#include <M5Unified.h>
 
 //---------------------
 #define SOUND_TWOPWM
@@ -65,11 +77,6 @@ void IRAM_ATTR OnTimer() {
 }
 
 //---------------------
-float CalcFrequency(float note) {
-  return 440.0f * pow(2, (note - (69.0f-12.0f))/12.0f);
-}
-
-//---------------------
 class System {
 public:
   void initialize() {
@@ -95,6 +102,51 @@ public:
       setLed(CRGB(0, 0, 0));
       delay(200);
     }
+    Wire.begin(I2CPIN_SDA, I2CPIN_SCL, I2C_FREQ);
+    {
+      M5.Lcd.setTextColor(TFT_WHITE);
+      M5.Lcd.setTextSize(2);
+
+      Wire.beginTransmission(LPS33_ADDR);
+      Wire.write(LPS33_WHOAMI_REG);
+      Wire.endTransmission();
+      Wire.requestFrom(LPS33_ADDR, 1);
+      uint8_t whoami = Wire.read();
+      if (whoami != LPS33_WHOAMI_RET) {
+        while (1) {
+          setLed(CRGB(100, 0, 0));
+          delay(200);
+          setLed(CRGB(0, 0, 0));
+          delay(200);
+        }
+      }
+      Wire.beginTransmission(LPS33_ADDR);
+      Wire.write(LPS33_REG_CTRL);
+      Wire.write(0x50); // odr (0x50=75Hz, 0x40=50Hz, 0x30=25Hz, 0x20=10Hz, 0x10=1Hz, 0x00=OFF)
+      Wire.endTransmission();
+      delay(100);
+      m_defaultPressure = GetPressure(0);
+#if 0
+      while (1) {
+        delay(100);
+        Wire.beginTransmission(LPS33_ADDR);
+        Wire.write(LPS33_REG_OUT_XL);
+        Wire.endTransmission();
+        Wire.requestFrom(LPS33_ADDR, 3);
+
+        uint8_t data[3];
+        for (int i = 0; i < 3; i++) {
+          data[i] = Wire.read();
+        }        
+        uint32_t pressure = ((((uint32_t)data[2]) << 16) | (((uint32_t)data[1]) << 8) | data[0]);
+        M5.Lcd.clear(TFT_DARKGREEN);
+        M5.Lcd.setCursor(0, 0);
+        M5.Lcd.printf("%02X %02X %02x\n", data[2], data[1], data[0]);
+        M5.Lcd.printf("%4.1f hPa", pressure / 4096.0f);
+      }
+#endif
+    }
+
     pinMode(PWMPIN_LOW, OUTPUT);
     pinMode(PWMPIN_HIGH, OUTPUT);
     pinMode(LEDPIN, OUTPUT);
@@ -111,40 +163,75 @@ public:
 
     timer = timerBegin(80*1000*1000 / CLOCK_DIVIDER);
     timerAttachInterrupt(timer, &OnTimer);
-    timerAlarm(timer, TIMER_ALARM, true, 0);
+    timerAlarm(timer, TIMER_ALARM, true, 0);    
   }
   bool m_btnPressed = false;
   void SetTickCount(float v) {
     m_tickCount = v;
   }
+  void SetVolume(float v) {
+    m_volume = v;
+  }
+  float GetVolume() const {
+    return m_volume;
+  }
 
 private:
   float m_tickCount = 0.0f;
+  float m_volume = 0.0f;
+  float m_defaultPressure = 0.0f;
+
+  float GetPressure(uint8_t side) {
+    Wire.beginTransmission(LPS33_ADDR + side);
+    Wire.write(LPS33_REG_OUT_XL);
+    Wire.endTransmission();
+    Wire.requestFrom(LPS33_ADDR + side, 3);
+
+    uint8_t data[3];
+    for (int i = 0; i < 3; i++) {
+      data[i] = Wire.read();
+    }        
+    uint32_t pressure = ((((uint32_t)data[2]) << 16) | (((uint32_t)data[1]) << 8) | data[0]);
+    return pressure / 256.0f;
+  }
+
+  static float CalcFrequency(float note) {
+    return 440.0f * pow(2, (note - (69.0f-12.0f))/12.0f);
+  }
 
   static void UpdateTask(void *parameter) {
     System *pSystem = static_cast<System *>(parameter);
-    for (;;) {
+    while (1) {
       M5.update();
+
+      float p = pSystem->GetPressure(0);
+      float v = (p - pSystem->m_defaultPressure) / 500.0f;
+      v *= v;
+      if (v < 0.0f) {
+        v = 0.0f;
+      }
+      else if (v > 1.0f) {
+        v = 1.0f;
+      }
+
       if (M5.BtnA.isPressed()) {
         pSystem->m_btnPressed = true;
         //noteOn(60, 127);
         //setLed(CRGB(0, 100, 0));
         //delay(500);
         //noteOff();
+        v = 0.2f;
       }
       else {
         pSystem->m_btnPressed = false;
       }
+      pSystem->SetVolume(v);
+      float wavelength = CalcFrequency(60);
+      pSystem->SetTickCount(wavelength / SAMPLING_RATE);
+
       static int count = 0;
-      if (pSystem->m_btnPressed) {
-        setLed(CRGB(0, 0, 100));
-        float wavelength = CalcFrequency(60);
-        pSystem->SetTickCount(wavelength / SAMPLING_RATE);
-      }
-      else {
-        setLed(CRGB(0, 0, (count < 60 ? count : 120-count)));
-        count = (count + 1) % 120;
-      }
+      setLed(CRGB(0, 0, (count < 60 ? count : 120-count)));
+      count = (count + 1) % 120;
       vTaskDelay(pdMS_TO_TICKS(10));
     }
   }
@@ -152,17 +239,16 @@ private:
   static void CreateWaveTask(void *parameter) {
     System *pSystem = static_cast<System *>(parameter);
     float phase = 0.0f;
-    for (;;) {
+    while (1) {
       uint32_t data;
       xTaskNotifyWait(0, 0, &data, portMAX_DELAY);
 
       phase += pSystem->m_tickCount;
       if (phase >= 1.0f) phase -= 1.0f;
 
-      uint16_t d = 32768;
-      if (pSystem->m_btnPressed) {
-        d = static_cast<uint16_t>(32768.0f - 15000.0f + 30000.0f * phase);
-      }
+      float f = 60000.0f * (pSystem->GetVolume() * (-0.5f + phase));
+      uint16_t d = static_cast<uint16_t>(32768.0f + f);
+
       uint8_t h = (d >> 8) & 0xFF;
       uint8_t l = d & 0xFF;
       portENTER_CRITICAL(&timerMux);
@@ -206,8 +292,11 @@ void loop() {
   //  setLed(CRGB(0, 0, 100));
   //  delay(500);
   //}
+  static int loopCount = 0;
+  M5.Lcd.clear(TFT_BLACK);
+  M5.Lcd.setCursor(0, 0);
+  float v = sys.GetVolume();
+  M5.Lcd.printf("%d\n%1.2f", loopCount++, v);
   delay(500);
-  ledcWriteChannel(5, 250);
-  delay(500);
-  ledcWriteChannel(5, 10);
+  ledcWriteChannel(5, (int)(v * 255));
 }
